88d4871858d3ebbcafef1815b5de8024bd2482ce
Diego Polaco <dieghard@gmail.com>
2021-05-31 19:04:07 -0300

    segundo Commit

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -4,6 +4,16 @@ exports.proyectoshome = (req, res) => {
 	});
 };
 
+exports.formularioProyecto = (req, res) => {
+	res.render("nuevoProyecto.pug", {
+		nombrePagina: "Nuevo Proyecto"
+	});
+};
+
+exports.nuevoProyecto = (req, res) => {
+	res.send("Enviaste el Formulario");
+};
+
 exports.nosotros = (req, res) => {
 	res.render("nostros.pug");
 };
